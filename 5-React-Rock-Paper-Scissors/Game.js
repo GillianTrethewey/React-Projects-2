5ba29457bd1a45bc342ad77d4982a193a7fed2d1
Gillian Trethewey <gilliantrethewey100@gmail.com>
2023-04-16 20:52:10 -0400

    css practice

---
+++ 5-React-Rock-Paper-Scissors/Game.js
@@ -0,0 +1,88 @@
+import React, { useState } from "react";
+import styles from './Game.module.css';
+
+const CHOICES = [
+  { name: "rock", emoji: "✊" },
+  { name: "paper", emoji: "✋" },
+  { name: "scissors", emoji: "✌️" },
+];
+
+const choiceStyles = {
+  display: "flex",
+  alignItems: "center",
+  marginBottom: 40,
+};
+
+const emojiStyles = {
+  fontSize: 64,
+  marginRight: 20,
+};
+
+const nameStyles = {
+  margin: 0,
+  fontSize: 24,
+  color: "#fff",
+};
+
+function Game() {
+  const [playerChoice, setPlayerChoice] = useState(null);
+  const [codeyChoice, setCodeyChoice] = useState(null);
+  const [result, setResult] = useState(null);
+
+  function handlePlayerChoice(choice) {
+    const codeyChoice = CHOICES[Math.floor(Math.random() * CHOICES.length)];
+    setPlayerChoice(choice);
+    setCodeyChoice(codeyChoice);
+    if (choice.name === codeyChoice.name) {
+      setResult("Tie!");
+    } else if (
+      (choice.name === "rock" && codeyChoice.name === "scissors") ||
+      (choice.name === "paper" && codeyChoice.name === "rock") ||
+      (choice.name === "scissors" && codeyChoice.name === "paper")
+    ) {
+      setResult("You win!");
+    } else {
+      setResult("You lose!");
+    }
+  }
+
+  function resetGame() {
+    setPlayerChoice(null);
+    setCodeyChoice(null);
+    setResult(null);
+  }
+
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.choices}>Rock Paper Scissors</h1>
+      <div>
+        {CHOICES.map((choice) => (
+          <button
+            key={choice.name}
+            onClick={() => handlePlayerChoice(choice)}
+            aria-label={choice.name}
+            style={{ fontSize: 40}}
+          >
+            {choice.emoji}
+          </button>
+        ))}
+      </div>
+      {playerChoice && codeyChoice && (
+        <div>
+          <div style={choiceStyles}>
+            <span style={emojiStyles}>{playerChoice.emoji}</span>
+            <p style={nameStyles}>You chose {playerChoice.name}</p>
+          </div>
+          <div style={choiceStyles}>
+            <span style={emojiStyles}>{codeyChoice.emoji}</span>
+            <p style={nameStyles}>The computer chose {codeyChoice.name}</p>
+          </div>
+          <h2 className={styles.results}>{result}</h2>
+          <button onClick={resetGame} className={styles.button}>Play again</button>
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default Game;
